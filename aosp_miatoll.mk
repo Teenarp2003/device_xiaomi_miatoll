98b8eae90ef410bed96822e9d8990a1ac96a5416
tusharind0074 <tusharredekar007@gmail.com>
2021-10-26 21:32:14 +0530

    miatoll: AOSPK bringup

--- aosp_miatoll.mk
+++ aosp_miatoll.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 The ArrowOS
+# Copyright (C) 2021 The The Kraken Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -7,20 +7,25 @@
 # Inherit from miatoll  device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
-# Inherit some common ArrowOS stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common Kraken stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier
-PRODUCT_NAME := arrow_miatoll
+PRODUCT_NAME := aosp_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Miatoll
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+WITH_GMS := true
+
+# BUILD_TYPE
+KRAKEN_BUILD_TYPE := OFFICIAL
 
 # Maintainer
-DEVICE_MAINTAINER := SonalSingh18
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.kraken.maintainer=TusharInd007
